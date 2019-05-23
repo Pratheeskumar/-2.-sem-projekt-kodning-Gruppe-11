46064426e9ff8accf3e98c422af9c8a361db60b0
Thomas Chan Lund Nielsen <thom8203@edu.eal.dk>
2019-05-24 01:39:08 +0200

    Update kort.js

--- Bolierplate/css/kort.js
+++ Bolierplate/css/kort.js
@@ -9,15 +9,6 @@
         }
     }
 
-    document.getElementById("tre1").addEventListener("click", myFunction);
-       function myFunction() {
-        var x = document.getElementById("sideBar");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-    }
 
     document.getElementById("krydset").addEventListener("click", myFunction);
     function myFunction() {
@@ -29,4 +20,24 @@
      }
  }
 
- 
+let sidebar = document.getElementById("sidebarleft")
+
+ document.getElementById("sideicon2").addEventListener("click", myFunction);
+ function myFunction() {
+     var x = document.getElementById("sidebarleft");
+     if (x.style.display === "none") {
+         x.style.display = "block";
+     } else {
+         x.style.display = "none";
+     }
+ }
+
+ document.getElementById("sideicon").addEventListener("click", myFunction);
+ function myFunction() {
+     var x = document.getElementById("sidebarleft");
+     if (x.style.display === "none") {
+         x.style.display = "block";
+     } else {
+         x.style.display = "none";
+     }
+ }
